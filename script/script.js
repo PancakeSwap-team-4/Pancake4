710b04660fde1385ce2a60d56c87f6376036f629
Alfio Puglisi <puglisi_algio@ciao.it>
2023-04-17 12:11:14 +0200

    random lotterynumber

--- script/script.js
+++ script/script.js
@@ -1,31 +1,11 @@
-const slides = document.querySelectorAll(".slide");
-const carousel = document.querySelector(".carousel");
-const indicators = document.querySelectorAll(".indicator");
-let currentIndex = 0;
+const lotteryNumber = document.querySelector('#lottery-number');
+const updateNumber = 10000;
 
-function goToSlide(index) {
-  if (index < 0 || index >= slides.length) return;
-
-  carousel.style.transform = `translateX(-${index * 20}%)`;
-
-  slides[currentIndex].classList.remove("active");
-  indicators[currentIndex].classList.remove("active");
-
-  slides[index].classList.add("active");
-  indicators[index].classList.add("active");
-
-  currentIndex = index;
+function randomNumber(){
+    const target = Math.floor(Math.random() * 100000);
+    const numberComma = target.toString().replace(/\B(?=(\d{3})+(?!\d))/g,',');
+    const dataVal = lotteryNumber.setAttribute("data-val", target);
+    return lotteryNumber.innerHTML = `$${numberComma}`;
 }
 
-function nextSlide() {
-  const nextIndex = (currentIndex + 1) % slides.length;
-  goToSlide(nextIndex);
-}
-
-setInterval(nextSlide, 100000000);
-
-indicators.forEach((indicator, index) => {
-  indicator.addEventListener("click", () => {
-    goToSlide(index);
-  });
-});
+setInterval(randomNumber, updateNumber);
