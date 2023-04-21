d8902244507a92fa7a4d4f463f893760419fd5bc
Alfio Puglisi <puglisi_algio@ciao.it>
2023-04-21 09:17:20 +0200

    counter numero lottery

--- script/script.js
+++ script/script.js
@@ -1,11 +1,30 @@
 const lotteryNumber = document.querySelector('#lottery-number');
-const updateNumber = 10000;
+const cardContentRight = document.querySelector('.win-card-content');
+const updateNumber = 5000;
+const speed = 300;
 
 function randomNumber(){
-    const target = Math.floor(Math.random() * 100000);
-    const numberComma = target.toString().replace(/\B(?=(\d{3})+(?!\d))/g,',');
-    const dataVal = lotteryNumber.setAttribute("data-val", target);
-    return lotteryNumber.innerHTML = `$${numberComma}`;
+    const target = Math.floor((Math.random() * 10000) + 80000);
+    let countStart = 0;
+    lotteryNumber.innerText = countStart;
+    console.log(lotteryNumber)
+    const myInterval = setInterval(() => {
+    if (countStart <= target - 4) {
+        countStart  += 104;
+        lotteryNumber.innerText = `$${countStart.toString().replace(/\B(?=(\d{3})+(?!\d))/g,',')}`;
+    } else {
+        countStart = target;
+        lotteryNumber.innerText = `$${countStart.toString().replace(/\B(?=(\d{3})+(?!\d))/g,',')}`;
+    clearInterval(myInterval);
+    }
+    console.log(myInterval);
+    }, 1/1000);
+
 }
 
-setInterval(randomNumber, updateNumber);
+window.addEventListener("load", randomNumber);
+
+function darkMode(){
+    let element = document.body;
+    element.classList.toggle("dark-mode");
+}
