47ae519ab95d6ebec44a03f9a052ed3d35910112
Louis Marcell Siegl <louis.marcel.siegl@hotmail.it>
2023-04-26 16:15:12 +0200

    Merge 26/04/2023

--- script/script.js
+++ script/script.js
@@ -1,31 +1,67 @@
-const slides = document.querySelectorAll(".slide");
-const carousel = document.querySelector(".carousel");
-const indicators = document.querySelectorAll(".indicator");
-let currentIndex = 0;
+const lotteryNumber = document.querySelector('#lottery-number');
+const cardContentRight = document.querySelector('.win-card-content');
 
-function goToSlide(index) {
-  if (index < 0 || index >= slides.length) return;
+//generiamo numeri random con counter
+function randomNumber(){
+    const target = Math.floor((Math.random() * 10000) + 80000);
+    let countStart = 0;
+    lotteryNumber.innerText = countStart;
+    console.log(lotteryNumber)
+    const myInterval = setInterval(() => {
+    if (countStart <= target - 4) {
+        countStart  += 104;
+        lotteryNumber.innerText = `$${countStart.toString().replace(/\B(?=(\d{3})+(?!\d))/g,',')}`;
+    } else {
+        countStart = target;
+        lotteryNumber.innerText = `$${countStart.toString().replace(/\B(?=(\d{3})+(?!\d))/g,',')}`;
+    clearInterval(myInterval);
+    }
+    console.log(myInterval);
+    }, 1/1000);
 
-  carousel.style.transform = `translateX(-${index * 20}%)`;
-
-  slides[currentIndex].classList.remove("active");
-  indicators[currentIndex].classList.remove("active");
-
-  slides[index].classList.add("active");
-  indicators[index].classList.add("active");
-
-  currentIndex = index;
 }
+window.addEventListener("load", randomNumber);
 
-function nextSlide() {
-  const nextIndex = (currentIndex + 1) % slides.length;
-  goToSlide(nextIndex);
-}
 
-setInterval(nextSlide, 100000000);
+// const h2Million = document.querySelectorAll('.million > h2');
+// const bgMillion = document.querySelector('.million-container');
+// const millionCard = document.querySelectorAll('.million-card');
+// const h2MillionCard = document.querySelectorAll('.text-description > h2:first-child');
+// const obliqueLine = document.querySelector('.million-oblique-line');
+// const winContainer = document.querySelector('.win-container');
+// const win = document.querySelector('.win');
+// const winH2 = document.querySelector('.win > h2:first-child');
+// const winButton = document.querySelectorAll('.win-button');
+// const svgButton = document.querySelectorAll('.win-button > svg');
 
-indicators.forEach((indicator, index) => {
-  indicator.addEventListener("click", () => {
-    goToSlide(index);
-  });
-});
+// function darkMode(){
+//     h2Million.forEach((item) => {
+//         item.classList.toggle('text-white');
+//     })
+
+//     bgMillion.classList.toggle('dark-million-container');
+
+//     millionCard.forEach((item) => {
+//         item.classList.toggle('dark-million-card');
+//     })
+
+//     h2MillionCard.forEach((item) => {
+//         item.classList.toggle('text-white');
+//     });
+
+//     obliqueLine.classList.toggle('dark-million-oblique-line');
+
+//     winContainer.classList.toggle('background-win-millions');
+
+//     win.classList.toggle('dark-win');
+
+//     winH2.classList.toggle('text-white');
+
+//     winButton.forEach((item) => {
+//         item.classList.toggle('dark-win-button');
+//     });
+
+//     svgButton.forEach((item) => {
+//         item.classList.toggle('dark-svg-win');
+//     })
+// }
